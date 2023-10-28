d675350092376243299633952654e7681d4c36f4
yunus-ujjaini <68804440+yunus-ujjaini@users.noreply.github.com>
2023-10-29 01:24:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { collection, getDocs, addDoc } from "firebase/firestore";   
+import { collection, getDocs, addDoc, updateDoc,doc } from "firebase/firestore";   
 import { useEffect, useState } from 'react';
 
 function App() {
   const [billNo,setBillNo]=useState(0);
-  const [date,setDate]=useState(new Date().toDateString().substring(4, 16));
+  const [sysdate,setsysDate]=useState(new Date().toDateString().substring(4, 16));
   const [time,setTime]=useState("Shift 1");
+  const [gweight,setGrossWeight]=useState("0 MTS");
+  const [tweight,setTareWeight]=useState("0 MTS");
+  const [nweight,setNetWeight]=useState("0 MTS");
+  const [goodsData,setGoodsData]=useState([]);
+  const [importerExporterData,setImporterExporterData]=useState([]);
+  const [vehicleData,setVehicleData]=useState([]);
   const firebaseConfig = {
     apiKey: "AIzaSyBXMUYnWGcGoljKmQYZJE2gML1_N1lEdx8",
     authDomain: "mahavirshipping-1b63e.firebaseapp.com",
@@ -25,7 +31,7 @@ function App() {
 
   async function fillInitialData(){
     //To fill Bill no
-    const querySnapshot = await getDocs(collection(db, "BillNo"));
+    let querySnapshot = await getDocs(collection(db, "BillNo"));
     querySnapshot.forEach((doc) => {
       setBillNo(doc.data().BillNo+1);
     });
@@ -33,27 +39,140 @@ function App() {
     //To fill Shift
     let time=new Date().toTimeString().substring(0,4);
     if(parseInt(time.split(":")[0])>=7 && parseInt(time.split(":")[0])<=15){
-      setTime("Shift 1");
+      setTime("1");
     }
     else if(parseInt(time.split(":")[0])>15 && parseInt(time.split(":")[0])<=23){
-      setTime("Shift 2");
+      setTime("2");
     }
     else if(parseInt(time.split(":")[0])>23 && parseInt(time.split(":")[0])<7){
-      setTime("Shift 3");
+      setTime("3");
     }
+
+    //Get Suggestion Options for Goods
+    querySnapshot = await getDocs(collection(db, "DescOfGoodsData"));
+    let SuggestionOptions=[];
+    querySnapshot.forEach((doc) => {
+      SuggestionOptions.push(doc.data().DescOfGoods);
+    });
+    setGoodsData(SuggestionOptions);
+
+    //Get Suggestion Options for Importer/Exporter
+    querySnapshot = await getDocs(collection(db, "ImporterExporterData"));
+    SuggestionOptions=[];
+    querySnapshot.forEach((doc) => {
+      SuggestionOptions.push(doc.data().ImporterExporter);
+    });
+    setImporterExporterData(SuggestionOptions);
   }
 
-  function saveData(){
+  function resetData(){
+    document.getElementById("GrossWeightInput").value="";
+    document.getElementById("TareWeightInput").value="";
+    document.getElementById("NetWeightInput").value="0.000 MTS";
+    document.getElementById("importerExporterInput").value="";
+    document.getElementById("qtyInput").value="";
+    document.getElementById("desOfGoodsInput").value="";
+    document.getElementById("truckInput").value="";
+    document.getElementById("vehicleInput").value="";
+    document.getElementById("driversNameInput").value="";
+    document.getElementById("billOfEntryInput").value="";
+    document.getElementById("billOfEntryDateInput").value="";
+    document.getElementById("warfageEntryInput").value="";
+    document.getElementById("warfageEntryDateInput").value="";
+    document.getElementById("vesselNameInput").value="";
+  }
+
+  function calNWeight() {
+    const cleanedGWeight = gweight.replace(/MTS|TS/gi, '').trim();
+    const cleanedTWeight = tweight.replace(/MTS|TS/gi, '').trim();
+
+    const grossWeight = parseFloat(cleanedGWeight);
+    const tareWeight = parseFloat(cleanedTWeight);
+
+    console.log(`Gross weight = ${gweight}\nTare weight = ${tweight}`);
+    let netWeight =(grossWeight - tareWeight).toFixed(3);
+    setNetWeight(`${netWeight} MTS`);
+    document.getElementById("qtyInput").value=`${netWeight} MTS`;
+}
+
+  async function saveData(){
+    let details={
+      No:billNo,
+      Date:sysdate,
+      Shift:`Shift - ${time}`,
+      GrossWeight:gweight,
+      TareWeight:tweight,
+      NetWeight:nweight,
+      ImporterExporter:document.getElementById("importerExporterInput").value,
+      Quantity:document.getElementById("qtyInput").value,
+      DescOfGoods:document.getElementById("desOfGoodsInput").value,
+      ByTruck:document.getElementById("truckInput").value,
+      VehicleNo:document.getElementById("vehicleInput").value,
+      DriversName:document.getElementById("driversNameInput").value,
+      BillOfEntry:document.getElementById("billOfEntryInput").value,
+      BillOfEntryDate:document.getElementById("billOfEntryDateInput").value,
+      WarfageEntry:document.getElementById("warfageEntryInput").value,
+      WarfageEntryDate:document.getElementById("warfageEntryDateInput").value,
+      VesselName:document.getElementById("vesselNameInput").value,
+    }
+    try {
+      const docRef = await addDoc(collection(db, "ActualData"), details);
+      console.log("Document written with ID: ", docRef.id);
+      
+      let fetchedData=await getDocs(collection(db, "DescOfGoodsData"));
+      let exists=false;
+      fetchedData.forEach(doc=>{
+        if(doc.data().DescOfGoods===details.DescOfGoods){
+            exists=true;
+        }
+      })
+      if(!exists){
+        const docRef = await addDoc(collection(db, "DescOfGoodsData"), {
+            DescOfGoods:details.DescOfGoods
+        });
+        console.log("Document written with ID: ", docRef.id);
+      }
+
+      fetchedData=await getDocs(collection(db, "ImporterExporterData"));
+      exists=false;
+      fetchedData.forEach(doc=>{
+        if(doc.data().ImporterExporter===details.ImporterExporter){
+            exists=true;
+        }
+      })
+      if(!exists){
+        const docRef = await addDoc(collection(db, "ImporterExporterData"), {
+            ImporterExporter:details.ImporterExporter
+        });
+        console.log("Document written with ID: ", docRef.id);
+      }
+
+      await updateDoc(doc(db, "BillNo", "BillNoDoc"), {
+        BillNo: details.No
+      });
+      
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
+  function saveAndPrintData(){
+    saveData();
     
   }
+
 
   useEffect(()=>{
     fillInitialData();
   },[])
 
+  useEffect(()=>{
+    calNWeight();
+  },[gweight,tweight])
+
 
   return (
     <div className="App">
+        <div className='Container'>
       <div className="head">
           <h1>MAHAVIR SHIPPING SERVICE</h1>
           <h3>Plot No. 295, Ward 6/C, Opp. Om Cineplex, Adipur - Kutch, 370205</h3>
@@ -75,7 +194,7 @@ function App() {
                       Date :
                   </h4>
                   <h4>
-                      {date}
+                      {sysdate}
                   </h4>
               </div>
               <div className="middle-right-input">
@@ -124,15 +243,19 @@ function App() {
           <div className="main-head-right">
               <div className="main-head-right-weight">
                   <h4>G. Wt.</h4>
-                  <input className="main-head-right-weight-gweight"/>
+                  <input className="main-head-right-weight-gweight" id='GrossWeightInput' placeholder="0.00 MTS" onChange={(e)=>{
+                    setGrossWeight(e.target.value)
+                  }} />
               </div>
               <div className="main-head-right-weight">
                   <h4>T. Wt.</h4>
-                  <input className="main-head-right-weight-tweight"/>
+                  <input className="main-head-right-weight-tweight"  id='TareWeightInput' placeholder="0.00 MTS" onChange={(e)=>{
+                    setTareWeight(e.target.value)
+                  }} />
               </div>
               <div className="main-head-right-weight">
                   <h4>N. Wt.</h4>
-                  <input className="main-head-right-weight-nweight"/>
+                  <input className="main-head-right-weight-nweight"  id='NetWeightInput' disabled value={nweight}/>
               </div>
           </div>
       </div>
@@ -143,36 +266,46 @@ function App() {
           <div className="main-form">
               <div className="main-form-input">
                   <h4>1. Name of Importer/Exporter</h4>
-                  <input/>
+                  <input list='importerExporters' id='importerExporterInput'/>
+                  <datalist id="importerExporters">
+                    {importerExporterData.map((val,key)=>{
+                      return <option value={val} key={key}/>
+                    })}
+                  </datalist>
               </div>
               <div className="main-form-input">
                   <h4>2. Quantity/No. of Packages</h4>
-                  <input/>
+                  <input id="qtyInput"/>
               </div>
               <div className="main-form-input">
                   <h4>3. Des. of goods</h4>
-                  <input/>
+                  <input list='goods' id="desOfGoodsInput" />
+                  <datalist id="goods">
+                    {goodsData.map((val,key)=>{
+                      return <option value={val} key={key}/>
+                    })}
+                  </datalist>
               </div>
               <div className="main-form-input">
                   <h4>4. By Truck</h4>
-                  <input/>
+                  <input id='truckInput'/>
               </div>
               <div className="main-form-input">
                   <h4>5. R.T.P. No./Vehicle No.</h4>
-                  <input/>
+                  <input id='vehicleInput'/>
               </div>
               <div className="main-form-input">
                   <h4>6. Driver's Name</h4>
-                  <input/>
+                  <input id='driversNameInput'/>
               </div>
               <div className="main-form-input main-form-input-spl">
                   <div className="main-form-input main-form-input-spl-input">
                       <h4>7. Bill of Entry/Shipping Bill/T.P. No.</h4>
-                      <input/>
+                      <input id='billOfEntryInput'/>
                   </div>
                   <div className="main-form-input main-form-input-spl-date">
-                      <h4>dt.</h4>
-                      <input/>
+                      <h4>Dt.</h4>
+                      <input type="date" id='billOfEntryDateInput'/>
                   </div>
                   
                   
@@ -180,18 +313,18 @@ function App() {
               <div className="main-form-input main-form-input-spl">
                   <div className="main-form-input main-form-input-spl-input">
                       <h4>8. Wharfage Entry No.</h4>
-                      <input/>
+                      <input id='warfageEntryInput'/>
                   </div>
                   
                   <div className="main-form-input main-form-input-spl-date">
-                      <h4>dt.</h4>
-                      <input/>
+                      <h4>Dt.</h4>
+                      <input type="date" id='warfageEntryDateInput'/>
                   </div>
                   
               </div>
               <div className="main-form-input">
                   <h4>9. Vessel's Name</h4>
-                  <input/>
+                  <input id='vesselNameInput'/>
               </div>
           </div>
           <div className="main-footer">
@@ -219,9 +352,24 @@ function App() {
               </div>
           </div>
       </div>
-      <button onClick={saveData}>
-        Save and Print
-      </button>
+      </div>
+
+      <div className='ActionButtons'>
+        <button onClick={()=>{
+            saveData();
+            resetData();
+            fillInitialData();
+            }}>
+          Save
+        </button>
+        <button onClick={saveAndPrintData}>
+          Save and Print
+        </button>
+        <button onClick={resetData}>
+          Reset Data
+        </button>
+      </div>
+      
     </div>
   );
 }
